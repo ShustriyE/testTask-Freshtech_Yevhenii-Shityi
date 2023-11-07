8bd854a8265643666035c32b77cb4ecbcdf5d9b0
ShustriyE <shutiyevgeniy@gmail.com>
2023-11-07 15:55:44 +0200

    separation of filters into components

--- src/components/table/FiltersWrapper.vue
+++ src/components/table/FiltersWrapper.vue
@@ -1,122 +1,48 @@
 <template>
   <div class="wrapper">
-    <div class="wrapper">
-      <h3>{{$t('include_filters')}}</h3>
-      <div>
-        <div
-          v-for="(rule, index) in includeFilters"
-          :key="index"
-          class="relative_wrapper filters_added"
-        >
-          <span v-for="(value, key) in rule" :key="key"
-            >{{ key }}: {{ value }}
-          </span>
-          <div @click="removeIncludeFilter(index)" class="button remove">+</div>
-        </div>
-      </div>
-      <div
-        v-if="showIncludeFiltersInput"
-        class="relative_wrapper inputs_wrapper"
-      >
-        <div
-          v-for="(field, index) in inputFields"
-          :key="index"
-          class="relative_wrapper inputs_wrapper__fields"
-        >
-          <select v-model="field.filterIncludeId">
-            <option disabled>{{$t('сhoose_option')}}</option>
-            <option v-for="id in chooseField" :key="id" :value="id">
-              {{ id }}
-            </option>
-          </select>
-          <input type="text" v-model="field.filterIncludeText" />
-          <div @click="removeIncludeField(index)" class="button remove_filter">
-            +
-          </div>
-        </div>
-        <div class="buttons_filter__wrapper">
-          <div @click="addIncludeFilter" class="button add">{{$t('add_filters')}}</div>
-          <div @click="addIncludeField" class="button add">{{$t('add_field')}}</div>
-        </div>
-      </div>
-      <div @click="showFiltersInputInclude" class="button add">
-        {{
-          showIncludeFiltersInput
-            ? $t('close_filter_settings')
-            : $t('add_include_filter')
-        }}
-      </div>
-    </div>
-    <div class="wrapper">
-      <h3>{{$t('exclude_filters')}}</h3>
-      <div>
-        <div
-          v-for="(rule, index) in excludeFilters"
-          :key="index"
-          class="relative_wrapper filters_added"
-        >
-          <span v-for="(value, key) in rule" :key="key"
-            >{{ key }}: {{ value }}
-          </span>
-          <div @click="removeExcludeFilter(index)" class="button remove">+</div>
-        </div>
-      </div>
-      <div
-        v-if="showExcludeFiltersInput"
-        class="relative_wrapper inputs_wrapper"
-      >
-        <div
-          v-for="(field, index) in excludeFields"
-          :key="index"
-          class="relative_wrapper"
-        >
-          <select v-model="field.filterExcludeId">
-            <option disabled>{{$t('сhoose_option')}}</option>
-            <option v-for="id in chooseField" :key="id" :value="id">
-              {{ id }}
-            </option>
-          </select>
-          <input type="text" v-model="field.filterExcludeText" />
-          <div @click="removeExcludeField(index)" class="button remove_filter">
-            +
-          </div>
-        </div>
-        <div class="buttons_filter__wrapper">
-          <div @click="addExcludeFilter" class="button add">{{$t('add_filters')}}</div>
-          <div @click="addExcludeField" class="button add">{{$t('add_field')}}</div>
-        </div>
-      </div>
-      <div @click="showFiltersInputExclude" class="button add">
-        {{
-          showExcludeFiltersInput
-            ? $t('close_filter_settings')
-            : $t('add_exclude_filter')
-        }}
-      </div>
-    </div>
+
+    <FilterBlock
+    :headerText="'include_filters'"
+    :buttonText="'add_include_filter'"
+    :filtersList="includeFilters"
+    :fieldsList="includeFields"
+    @removeFilter="removeIncludeFilter"
+    @addFilter="addIncludeFilter"
+    @removeField="removeIncludeField"
+    @addField="addIncludeField"
+    />
+
+    <FilterBlock
+    :headerText="'exclude_filters'"
+    :buttonText="'add_exclude_filter'"
+    :filtersList="excludeFilters"
+    :fieldsList="excludeFields"
+    @removeFilter="removeExcludeFilter"
+    @addFilter="addExcludeFilter"
+    @removeField="removeExcludeField"
+    @addField="addExcludeField"
+    />
+
   </div>
 </template>
 
 <script>
+import FilterBlock from '../unevrsal-components/FilterBlock.vue'
+
 export default {
   name: "FiltersWrapper",
+  components: {FilterBlock},
   data() {
     return {
-      filterIncludeId: "",
-      filterIncludeText: "",
-      filterExcludeId: "",
-      filterExcludeText: "",
-      showIncludeFiltersInput: false,
-      showExcludeFiltersInput: false,
-      inputFields: [{ filterIncludeId: "", filterIncludeText: "" }],
-      excludeFields: [{ filterExcludeId: "", filterExcludeText: "" }],
+      includeFields: [{ filterId: "", filterText: "" }],
+      excludeFields: [{ filterId: "", filterText: "" }],
     }
   },
   methods: {
     formatFiltersRule(obj) {
       const result = obj.reduce((result, currentObject) => {
-        const { filterIncludeId, filterIncludeText } = currentObject
-        result[filterIncludeId] = filterIncludeText
+        const { filterId, filterText } = currentObject
+        result[filterId] = filterText
         return result
       }, {})
       if (Object.values(result).every((value) => value.length > 0)) {
@@ -126,16 +52,16 @@ export default {
     addIncludeFilter() {
       this.$store.dispatch(
         "addIncludeFilterRule",
-        this.formatFiltersRule(this.inputFields)
+        this.formatFiltersRule(this.includeFields)
       )
-      this.inputFields = [{ filterIncludeId: "", filterIncludeText: "" }]
+      this.includeFields = [{ filterId: "", filterText: "" }]
     },
     addExcludeFilter() {
       this.$store.dispatch(
         "addExcludeFilterRule",
-        this.formatFiltersRule(this.inputFields)
+        this.formatFiltersRule(this.excludeFields)
       )
-      this.excludeFields = [{ filterExcludeId: "", filterExcludeText: "" }]
+      this.excludeFields = [{ filterId: "", filterText: "" }]
     },
     removeIncludeFilter(index) {
       this.$store.dispatch("removeIncludeFilterRule", index)
@@ -143,32 +69,26 @@ export default {
     removeExcludeFilter(index) {
       this.$store.dispatch("removeExcludeFilterRule", index)
     },
-    showFiltersInputInclude() {
-      this.showIncludeFiltersInput = !this.showIncludeFiltersInput
-    },
-    showFiltersInputExclude() {
-      this.showExcludeFiltersInput = !this.showExcludeFiltersInput
-    },
     addIncludeField() {
-      this.inputFields.push([{ filterIncludeId: "", filterIncludeText: "" }])
-    },
-    removeIncludeField(index) {
-      if (this.inputFields.length > 1) {
-        this.inputFields.splice(index, 1)
-      } else {
-        this.inputFields = [{ filterIncludeId: "", filterIncludeText: "" }]
-        this.showIncludeFiltersInput = false
-      }
+      this.includeFields.push([{ filterId: "", filterText: "" }])
     },
     addExcludeField() {
-      this.excludeFields.push([{ filterExcludeId: "", filterExcludeText: "" }])
+      this.excludeFields.push([{ filterId: "", filterText: "" }])
+    },
+    removeIncludeField(index) {
+      if (this.includeFields.length > 1) {
+        this.includeFields.splice(index, 1)
+      } else {
+        this.includeFields = [{ filterId: "", filterText: "" }]
+        this.showIncludeFilters = false
+      }
     },
     removeExcludeField(index) {
       if (this.excludeFields.length > 1) {
         this.excludeFields.splice(index, 1)
       } else {
-        this.excludeFields = [{ filterExcludeId: "", filterExcludeText: "" }]
-        this.showExcludeFiltersInput = false
+        this.excludeFields = [{ filterId: "", filterText: "" }]
+        this.showIncludeFilters = false
       }
     },
   },
@@ -179,94 +99,16 @@ export default {
     excludeFilters() {
       return this.$store.getters.filterRules.exclude
     },
-    formFields() {
-      return this.$store.getters.formFields
-    },
-    chooseField() {
-      return this.formFields.map((field) => field.id)
-    },
   },
 };
 </script>
 
 <style lang="scss">
-.relative_wrapper {
-  position: relative;
-}
-
 .wrapper {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: flex-start;
   width: 50vw;
-}
-
-.buttons_filter__wrapper {
-  display: flex;
-  flex-direction: row;
-  justify-content: space-evenly;
-  align-items: center;
-  display: flex;
-  align-items: center;
-  justify-content: space-around;
-  margin: 10px 0;
-
-  :nth-child(2) {
-    margin-left: 5px;
-  }
-}
-
-.filters_added {
-  margin: 5px;
-}
-
-.inputs_wrapper {
-  min-width: 200px;
-
-  &__fields {
-    display: flex;
-    flex-direction: row;
-    justify-content: space-evenly;
-    align-items: center;
-    width: 30vw;
-    margin: 5px 0;
-
-    input {
-      margin-left: 5px;
-    }
-  }
-}
-
-.button {
-  user-select: none;
-  padding: 5px;
-  text-align: center;
-  cursor: pointer;
-}
-
-.add {
-  border: 1px solid black;
-  border-radius: 5px;
-}
-
-.remove {
-  position: absolute;
-  left: -13px;
-  top: -5px;
-  rotate: 50deg;
-  width: 1px;
-  font-size: 21px;
-  color: red;
-
-  &_filter {
-    position: absolute;
-    left: 345px;
-    top: -5px;
-    rotate: 50deg;
-    width: 1px;
-    font-size: 21px;
-    color: red;
-  }
 }
 </style>
---
+++ src/components/unevrsal-components/FilterBlock.vue
@@ -0,0 +1,193 @@
+<template>
+<div class="wrapper">
+      <h3>{{$t(headerText)}}</h3>
+      <div>
+        <div
+          v-for="(rule, index) in filtersList"
+          :key="index"
+          class="relative_wrapper filters_added"
+        >
+          <span v-for="(value, key) in rule" :key="key"
+            >{{ key }}: {{ value }}
+          </span>
+          <div @click="removeFilter(index)" class="button remove">+</div>
+        </div>
+      </div>
+      <div
+        v-if="showFiltersInput"
+        class="relative_wrapper inputs_wrapper"
+      >
+        <div
+          v-for="(field, index) in fieldsList"
+          :key="index"
+          class="relative_wrapper inputs_wrapper__fields"
+        >
+          <select v-model="field.filterId">
+            <option disabled>{{$t('сhoose_option')}}</option>
+            <option v-for="id in chooseField" :key="id" :value="id">
+              {{ id }}
+            </option>
+          </select>
+          <input type="text" v-model="field.filterText" />
+          <div @click="removeField(index)" class="button remove_filter">
+            +
+          </div>
+        </div>
+        <div class="buttons_filter__wrapper">
+          <div @click="addFilter" class="button add">{{$t('add_filters')}}</div>
+          <div @click="addField" class="button add">{{$t('add_field')}}</div>
+        </div>
+      </div>
+      <div @click="setShowFiltersInput" class="button add">
+        {{
+          showFiltersInput
+            ? $t('close_filter_settings')
+            : $t(buttonText)
+        }}
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "FilterBlock",
+  props: {
+    headerText: {
+      type: String,
+      required: true,
+    },
+    buttonText: {
+      type: String,
+      required: true,
+    },
+    filtersList: {
+      type: Array,
+      required: true,
+    },
+    fieldsList: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      showFiltersInput: false,
+    }
+  },
+  computed: {
+    currentValue: {
+      get() {
+        return this.value
+      },
+      set(newValue) {
+        this.$emit("input", newValue)
+      },
+    },
+    formFields() {
+      return this.$store.getters.formFields
+    },
+    chooseField() {
+      return this.formFields.map((field) => field.id)
+    },
+  },
+  methods: {
+    addFilter() {
+      this.$emit('addFilter')
+    },
+    removeFilter(index) {
+      this.$emit('removeFilter', index)
+    },
+    addField() {
+      this.$emit('addField')
+    },
+    removeField(index) {
+      this.$emit('removeField', index)
+    },
+    setShowFiltersInput() {
+      this.showFiltersInput = !this.showFiltersInput
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.relative_wrapper {
+  position: relative;
+}
+
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  width: 50vw;
+}
+
+.buttons_filter__wrapper {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  margin: 10px 0;
+
+  :nth-child(2) {
+    margin-left: 5px;
+  }
+}
+
+.filters_added {
+  margin: 5px;
+}
+
+.inputs_wrapper {
+  min-width: 200px;
+
+  &__fields {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+    align-items: center;
+    width: 30vw;
+    margin: 5px 0;
+
+    input {
+      margin-left: 5px;
+    }
+  }
+}
+
+.button {
+  user-select: none;
+  padding: 5px;
+  text-align: center;
+  cursor: pointer;
+}
+
+.add {
+  border: 1px solid black;
+  border-radius: 5px;
+}
+
+.remove {
+  position: absolute;
+  left: -18px;
+  top: -10px;
+  rotate: 50deg;
+  width: 1px;
+  font-size: 21px;
+  color: red;
+
+  &_filter {
+    position: absolute;
+    left: 340px;
+    top: -8px;
+    rotate: 50deg;
+    width: 1px;
+    font-size: 21px;
+    color: red;
+  }
+}
+</style>
